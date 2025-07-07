3f6ebeb6bf05d77f3eb9baa8d849fddb6f7c8e8e
Dorff-th <nice4217@naver.com>
2025-07-07 10:08:47 +0900

    Remove frontend-vue from Git tracking

--- frontend-vue/src/views/admin/Dashboard.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-4">
-    <h1 class="text-xl font-bold">관리자 대시보드</h1>
-  </div>
-</template>
-
-<script setup>
-// 관리자 대시보드 화면
-</script>
--- frontend-vue/src/views/user/MyPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-4">
-    <h1 class="text-xl font-bold">마이페이지</h1>
-  </div>
-</template>
-
-<script setup>
-// 마이페이지 화면
-</script>
